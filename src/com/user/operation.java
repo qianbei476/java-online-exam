627cf336d7d7caff6b698b5bf2d0276529a9d56e
qianbei476 <584854762@qq.com>
2016-12-19 19:42:13 +0800

    Update operation.java

--- src/com/user/operation.java
+++ src/com/user/operation.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.struts2.ServletActionContext;
 import com.beans.STATUS;
@@ -19,6 +20,7 @@ public class operation extends ActionSupport{
 	private static final long serialVersionUID = 1L;
 	public static String FAILED = "404";
 	private UserDao userDao = new UserDaoImp();
+	HttpSession session = ServletActionContext.getRequest().getSession();
 	
 	public static STATUS status = new STATUS();	//初始化状态 
 	public String GsonToJsonStr(Object obj){
@@ -41,10 +43,14 @@ public class operation extends ActionSupport{
 		String password = request.getParameter("password");
 		User user = new User(username, password);
 		status.setUSERNAME(username);
+		
 		if(userDao.Userlogin(user)){		
 			status.setValue("succeed");
+			session.setAttribute("USER", user);
+			System.out.println(session.getAttribute("USER"));//test
 		}else {
 			status.setValue("failed");
+			session.removeAttribute("USER");//销毁session
 		}
 		//System.out.println("1"+status.toString());
 //		request.getSession().setAttribute("curUser", user);
@@ -52,14 +58,6 @@ public class operation extends ActionSupport{
 //		ActionContext.getContext().put(key, value)
 		//System.out.println(GsonToJsonStr(status));
 		return SUCCESS;
-	}
-	
-	public String UserLoginLink(){
-		//System.out.println("2"+status.toString());
-		if((status.getType().equals("login")||(status.getType().equals("userinfo")))&&status.getValue().equals("succeed"))
-			return SUCCESS;
-		else 
-			return "LOGINBACK";
 	}
 	
 	
@@ -95,16 +93,9 @@ public class operation extends ActionSupport{
 		return SUCCESS;
 	}
 	
-	public String UserInfo(){
-		status.setType("userinfo");
-		status.setValue("succeed");
-		return SUCCESS;
-	}
-	
 	public String UserLogout(){
-		status.setUSERNAME(null);
-		status.setType("logout");
-		status.setValue("succede");
+		status = null;
+		session.removeAttribute("USER");
 		return SUCCESS;
 	}
 	
